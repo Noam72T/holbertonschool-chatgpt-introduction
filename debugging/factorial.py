a7c5d3bb128c2ccdd5af1c5d99a8b42637372763
Noam72T <9823@holbertonstudents.com>
2025-01-14 09:02:58 +0100

    Factorial Fix Betty

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
 import sys
 
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n to avoid infinite loop
+        n -= 1  # Décrémenter n pour éviter la boucle infinie
     return result
 
 f = factorial(int(sys.argv[1]))
 print(f)
+
