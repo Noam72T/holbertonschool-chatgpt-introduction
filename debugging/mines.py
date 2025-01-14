1fa18a00e70563f031ba864a6fdf3dfddf671b0e
Noam72T <9823@holbertonstudents.com>
2025-01-14 09:18:37 +0100

    Mines Fix

--- debugging/mines.py
+++ debugging/mines.py
@@ -9,15 +9,19 @@ class Minesweeper:
     def __init__(self, width=10, height=10, mines=10):
         self.width = width
         self.height = height
-        self.mines = set(random.sample(range(width * height), mines))
+        self.total_cells = width * height
+        self.num_mines = mines
+        self.mines = set(random.sample(range(self.total_cells), mines))
         self.field = [[' ' for _ in range(width)] for _ in range(height)]
         self.revealed = [[False for _ in range(width)] for _ in range(height)]
+        self.non_mine_cells = self.total_cells - self.num_mines
+        self.revealed_cells = 0  # Track the number of revealed non-mine cells
 
     def print_board(self, reveal=False):
         clear_screen()
         print('  ' + ' '.join(str(i) for i in range(self.width)))
         for y in range(self.height):
-            print(str(y) + ' ', end='')
+            print(y, end=' ')
             for x in range(self.width):
                 if reveal or self.revealed[y][x]:
                     if (y * self.width + x) in self.mines:
@@ -40,9 +44,12 @@ class Minesweeper:
         return count
 
     def reveal(self, x, y):
+        if self.revealed[y][x]:  # If already revealed, do nothing
+            return True
         if (y * self.width + x) in self.mines:
             return False
         self.revealed[y][x] = True
+        self.revealed_cells += 1
         if self.count_mines_nearby(x, y) == 0:
             for dx in [-1, 0, 1]:
                 for dy in [-1, 0, 1]:
@@ -51,26 +58,18 @@ class Minesweeper:
                         self.reveal(nx, ny)
         return True
 
-    def all_safe_cells_revealed(self):
-        for y in range(self.height):
-            for x in range(self.width):
-                if (y * self.width + x) not in self.mines and not self.revealed[y][x]:
-                    return False
-        return True
-
     def play(self):
         while True:
             self.print_board()
+            if self.revealed_cells == self.non_mine_cells:  # Check for win condition
+                print("Congratulations! You've cleared the board without hitting a mine!")
+                break
             try:
                 x = int(input("Enter x coordinate: "))
                 y = int(input("Enter y coordinate: "))
                 if not self.reveal(x, y):
                     self.print_board(reveal=True)
                     print("Game Over! You hit a mine.")
-                    break
-                if self.all_safe_cells_revealed():
-                    self.print_board(reveal=True)
-                    print("Congratulations! You've won the game.")
                     break
             except ValueError:
                 print("Invalid input. Please enter numbers only.")
