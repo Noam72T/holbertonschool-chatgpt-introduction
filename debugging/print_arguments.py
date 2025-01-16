2c7624b35c8267e794d116e6c60e02913a6e95d6
Noam72T <9823@holbertonstudents.com>
2025-01-16 08:52:41 +0100

    Print Argument Fix

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+
 import sys
 
-if len(sys.argv) > 1:
-    for arg in sys.argv[1:]:  # Ignorer sys.argv[0]
-        print(arg)
-else:
-    print("No arguments were provided.")
+# Loop through arguments starting from index 1 to skip the script name
+for argument in sys.argv[1:]:
+    print(argument)
